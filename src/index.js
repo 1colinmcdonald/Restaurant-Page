c8a6ee9ba3a00a8170bfa9c79e5b8a601adfdc54
Colin McDonald <1colinmcdonald@gmail.com>
2024-08-15 21:30:34 -0700

    Add button styling

--- src/index.js
+++ src/index.js
@@ -3,28 +3,53 @@ import createHome from "./home/home.js";
 import createMenu from "./menu/menu.js";
 import createAbout from "./about/about.js";
 
+function removeClickedStylingFromTabs() {
+    for (const tab of document.querySelectorAll("nav>*")) {
+        tab.classList.remove("last-clicked");
+    }
+}
+
 export const pageContent = document.querySelector("#content");
 
+let lastTabClicked = "home";
 function clearPageContent() {
     while (pageContent.firstChild) {
         pageContent.removeChild(pageContent.lastChild);
     }
 }
 
-const homeButton = document.querySelector("#home");
+const homeButton = document.querySelector("#home-tab");
 homeButton.addEventListener("click", () => {
-    clearPageContent();
-    createHome();
+    if (lastTabClicked !== "home") {
+        lastTabClicked = "home";
+        removeClickedStylingFromTabs()
+        homeButton.classList.add("last-clicked");
+        clearPageContent();
+        createHome();
+    }
 });
 
-const menuButton = document.querySelector("#menu");
+const menuButton = document.querySelector("#menu-tab");
 menuButton.addEventListener("click", () => {
-    clearPageContent();
-    createMenu();
+    if (lastTabClicked !== "menu") {
+        lastTabClicked = "menu";
+        removeClickedStylingFromTabs()
+        menuButton.classList.add("last-clicked");
+        clearPageContent();
+        createMenu();
+    }
 });
 
-const aboutButton = document.querySelector("#about");
+const aboutButton = document.querySelector("#about-tab");
 aboutButton.addEventListener("click", () => {
-    clearPageContent();
-    createAbout();
+    if (lastTabClicked !== "about") {
+        lastTabClicked = "about";
+        removeClickedStylingFromTabs()
+        aboutButton.classList.add("last-clicked");
+        clearPageContent();
+        createAbout();
+    }
 });
+
+homeButton.classList.add("last-clicked");
+createHome();
--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,9 +1,32 @@
+import "./menu.css"
 export default function createMenu() {
     const menu = document.createElement("div");
     const title = document.createElement("h1");
     title.textContent = "Our Menu";
     menu.appendChild(title);
     const pageContent = document.querySelector("#content");
+    menu.appendChild(generateMenuItem("charcoal grilled steak", "duck fat potato pave, green beans, horseradish puree, charred leek & marrow butter", "| 8 oz prime picanha $47 | 12 oz prime NY Strip $66 | 16 oz prime ribeye $76"));
+    menu.appendChild(generateMenuItem("ash rubbed pork tenderloin | GF", "8 oz carton farms pork tenderloin, caramelized carrot puree, duck fat braised cabbage, pickled apple & pea vine slaw, pinot noir gastrique", "$32"));
+    menu.appendChild(generateMenuItem("duck breast | GF", "8 oz maple leaf duck breast, cherry & basil reduction, 5 spice sweet potato purée, swiss chard, polenta coin", "$37"));
+    menu.appendChild(generateMenuItem("scallops | GF", "swiss chard romanesco, pancetta, golden beet puree, jalapeno tartar", "$48"));
+    menu.id = "menu";
     pageContent.appendChild(menu);
-    
+}
+
+function generateMenuItem(name, description, prices) {
+    const menuItem = document.createElement("div");
+    const menuItemName = document.createElement("div");
+    menuItemName.classList.add("menu-item-name");
+    const menuItemDescription = document.createElement("div");
+    menuItemDescription.classList.add("menu-item-description");
+    const menuItemPrices = document.createElement("div");
+    menuItemPrices.classList.add("menu-item-prices");
+    menuItemName.textContent = name;
+    menuItemDescription.textContent = description;
+    menuItemPrices.textContent = prices;
+    menuItem.appendChild(menuItemName);
+    menuItem.appendChild(menuItemDescription);
+    menuItem.appendChild(menuItemPrices);
+    menuItem.classList.add("menu-item");
+    return menuItem;
 }
